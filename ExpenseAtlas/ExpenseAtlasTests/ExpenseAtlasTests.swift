f31240103285f00afcb31d6826e9c4b63ccc33a4
Harry Yan <harryyan1238@gmail.com>
2026-01-01 16:52:30 +1300

    Add empty project

---
+++ ExpenseAtlas/ExpenseAtlasTests/ExpenseAtlasTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseAtlasTests.swift
+//  ExpenseAtlasTests
+//
+//  Created by Harry Yan on 01/01/2026.
+//
+
+import Testing
+
+struct ExpenseAtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
